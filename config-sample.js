6b9e6a7115fbd85a2c3020adef97d2f6f42ae4d1
Lionel F <lionel.fristot@fullsix.com>
2015-02-27 19:14:57 +0100

    added configuration sample

---
+++ config-sample.js
@@ -0,0 +1,17 @@
+var botName = "plut⦿";
+var welcome = "\tHi @everyone, I\'m plut⦿ !\n";
+
+var config = {
+    'default': {
+	'botName': 	       botName,
+    'welcomeMessage':  welcome,
+	'org': 		       '',
+	'flow':            '',
+	'userToken':       '',
+	'apiToken':        '',
+    'jiraUser':        '',
+    'jiraPass':        ''
+	}
+};
+
+module.exports = config;
